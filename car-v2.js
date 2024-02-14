42958ebcfaec7ac7d89f9edba73a267770ff1b20
iremberber <iremberber0@gmail.com>
2024-02-15 01:42:26 +0300

    update

--- car-v2.js
+++ car-v2.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var box1 = document.querySelector('.box.box-1');
-    var container = document.querySelector('.container');
+    var title718 = document.querySelector('.title718');
+    var information = document.querySelector('.information1')
+    var carimg = document.querySelector('.carimg')
     
-    if(box1 && container) {
+    if(box1 && information) {
         box1.addEventListener('click', function() {
-            container.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            information.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            carimg.style.left = ('0px')
+            title718.style.left = ('100px')
         });
     }
+
 });
+
